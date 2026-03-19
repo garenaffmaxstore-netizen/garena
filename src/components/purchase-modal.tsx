21cfdd95f92d10f5f96ac5125c24425e1d7b9264
sayan2nd <sm1555524@egmail.com>
2026-03-19 08:21:14 +0000

    change the upi id to "freefire-india.mondal@yesg"

--- src/components/purchase-modal.tsx
+++ src/components/purchase-modal.tsx
@@ -371,7 +371,7 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
             </>
         );
     case 'qrPayment':
-        const upiId = "freefireindiastore@upi";
+        const upiId = "freefire-india.mondal@yesg";
         const upiUrl = `upi://pay?pa=${upiId}&pn=Garena&am=${finalPrice}&cu=INR&tn=${product.name}`;
         const minutes = Math.floor(qrCountdown / 60);
         const seconds = qrCountdown % 60;
